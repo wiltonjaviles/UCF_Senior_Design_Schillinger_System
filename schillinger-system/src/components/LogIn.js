8c0cdef4bc3e344dbdb1390c336627bc252c9cc4
Sleepie <malaniz21@knights.ucf.edu>
2021-02-03 19:13:18 -0500

    added login support for the backend

--- schillinger-system/src/components/LogIn.js
+++ schillinger-system/src/components/LogIn.js
@@ -1,21 +1,92 @@
 import '.././Style.css';
 import {Container, Form, Button} from 'react-bootstrap' 
+import React, { useState } from 'react';
 
-function LogIn() {
+const BASE_URL = 'http://localhost:5000/api/'
+
+export default function LogIn() {
+
+  const [state , setState] = useState({
+    loginUsername : "",
+    loginPassword : ""
+  })
+  const handleChange = (e) => {
+    const {id , value} = e.target   
+    setState(prevState => ({
+        ...prevState,
+        [id] : value
+    }))
+  }
+
+  const [message,setMessage] = useState('');
+
+  const doLogin = async event => 
+    {
+      event.preventDefault();     
+      if( state.loginUsername === '' || state.loginPassword === '' ){
+        setMessage('Please fill out all fields!');
+      }
+      else{
+        var js = 
+        '{"username":"' + state.loginUsername + 
+        '","password":"' + state.loginPassword 
+        +'"}';
+
+        try
+        {    
+            const response = await fetch(BASE_URL + 'login',
+            {
+              method:'POST',
+              body:js,
+              headers:
+              {
+                'Content-Type': 'application/json'
+              }
+            }
+            );
+
+            var res = JSON.parse(await response.text());
+
+            if( res.error !==  '' )
+            {
+                setMessage(res.error);
+            }
+            else if( res.jwt === '' )
+            {
+                setMessage('Token Error')
+            }
+            else
+            {
+                let user = {username:res.username}
+                let session = {token:res.jwt}
+                localStorage.setItem('user_data', JSON.stringify(user));
+                localStorage.setItem('session_token', JSON.stringify(session));
+
+                setMessage('Login Successful');
+                window.location.href = '/';
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }
+      } 
+    };
+
   return (
     <Container fluid>
       <Form>
-        <Form.Group controlId="formUsername">
+        <Form.Group controlId="loginUsername">
           <Form.Label>Log in. </Form.Label>
-          <Form.Control type="username" placeholder="username" />
+          <Form.Control type="username" placeholder="username" value={state.loginUsername} onChange={handleChange}/>
         </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Control type="password" placeholder="password" />
+        <Form.Group controlId="loginPassword">
+          <Form.Control type="password" placeholder="password" value={state.loginPassword} onChange={handleChange} />
         </Form.Group>
-        <Button variant="secondary" type="submit" className="float-right">Log In</Button>
+        <Button variant="secondary" type="submit" className="float-right" onClick={doLogin}>Log In</Button>
+        <span id="registerResult">{message}</span>
       </Form>
     </Container> 
   );
 }
-
-export default LogIn;
