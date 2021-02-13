53962265d3f3872a289b58de200e2c7cf548bc5e
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-02-13 12:52:06 -0500

    started generator form

---
+++ schillinger-system/src/components/applets/GeneratorForm.js
@@ -0,0 +1,15 @@
+import {Container, Row, Col, Form} from 'react-bootstrap' 
+import '../.././Style.css';
+
+function GeneratorForm() {
+  
+  return (
+    <Container>
+      <Form>
+        
+      </Form>
+    </Container>
+  );
+}
+
+export default GeneratorForm;
--- schillinger-system/src/components/books/BookOne.js
+++ schillinger-system/src/components/books/BookOne.js
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap';
 import TableOfContentsBar from '../TableOfContentsBar';
+import GeneratorForm from '../applets/GeneratorForm';
 import '../.././Style.css';
 
 function BookOne() {
@@ -9,6 +10,7 @@ function BookOne() {
         < TableOfContentsBar />
         <h1>Book One: Theory of Rhythm</h1>
         <a className="chapter-section-link" href="/book1#1.1" id="1.1"><h3>Chapter 1. Notation System</h3></a>
+        <GeneratorForm />
         <a className="chapter-section-link" href="/book1#1.1.a" id="1.1.a"><h5>A. Graphing Music</h5></a>
         <a className="chapter-section-link" href="/book1#1.1.b" id="1.1.b"><h5>B. Forms of Periodicity</h5></a>
         <a className="chapter-section-link" href="/book1#1.2" id="1.2"><h3>Chapter 2. Interferences of Periodicities</h3></a>
