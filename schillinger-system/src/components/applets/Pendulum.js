a51e22f07e6e2aaabefed35bb2ebeb81a98da35e
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-02-22 00:04:32 -0500

    working pendulum now only need to make inputs

--- schillinger-system/src/components/applets/Pendulum.js
+++ schillinger-system/src/components/applets/Pendulum.js
@@ -1,26 +1,36 @@
 import {Container, Row, Col, Card, Form} from 'react-bootstrap';
 import React from "react";
+import anime from "animejs";
 import '../.././Style.css';
 
 function Pendulum() {
-  
+
+  var angle = 80;
+  var duration = 1000;
+  const animationRef = React.useRef(null);
+  React.useEffect(() => {
+    animationRef.current = anime({
+      targets: '#rod',
+      rotate: [angle, -angle],
+      duration: duration,
+      easing: 'easeInOutSine',
+      direction: 'alternate',
+      loop: true
+    });      
+  }, []);
   return (
     <Container>
       <Card>
         <Card.Body>
           <h2>Pendulum Applet</h2>
-          <Form>
-            <Form.Group controlId="formLength">
-              <Form.Label>Length</Form.Label>
-              <Form.Control type="range" min="0.1" max="2" step="0.1" />
-            </Form.Group>
-            <Form.Group controlId="formPeriod">
-              <Form.Label>Angle</Form.Label>
-              <Form.Control type="range" min="1" max="179" step="1" />
-            </Form.Group>
-          </Form>
+          <br />
+          <div id="rod">
+            <div id="pivot"></div>
+            <div id="ball"></div>
+          </div>
         </Card.Body>
       </Card>
+      <br />
     </Container>
   );
 }
