6a7aa3f6e6339ca788240057aa04a1b597d01b2b
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-03-04 14:01:36 -0500

    working on the applet

--- schillinger-system/src/components/applets/Ch12Generator.js
+++ schillinger-system/src/components/applets/Ch12Generator.js
@@ -3,10 +3,67 @@ import React, { useState } from 'react';
 import '../.././Style.css';
 
 function Ch12Generator() {
+  
   const [state , setState] = useState({
-    formula : "",
+    warning: "",
+    input : "",
+    factorialFormula: "",
+    fractionalFormula: "",
+    factorialPlugIn: "",
+    fractionalPlugIn: "",
     result: ""
   })
+
+  const DistributivePower = (n, f) => {
+    if(n < 2) {
+      setState(prevState => ({
+        ...prevState,
+        warning: "You entry must be of the form (a+b+...+m)^n with at least two terms and n > 1"
+      }))
+      return;
+    }
+    if(n == 2) {
+      var factorialFormula = "(";
+      f.forEach(element => {
+        factorialFormula = factorialFormula.concat(String(element)+"+")
+      });
+      factorialFormula = factorialFormula.slice(0, -1);
+      factorialFormula = factorialFormula.concat(")^2 = ");
+      f.forEach(element => {
+        factorialFormula = factorialFormula.concat("(");
+        f.forEach(element2 => {
+          if(element == element2) {
+            var square = String(element)+"^2 + "
+            factorialFormula = factorialFormula.concat(square);
+          } else {
+            var mult = String(Number(element)*Number(element2))+" + ";
+            factorialFormula = factorialFormula.concat(mult);
+          }
+        });
+        factorialFormula = factorialFormula.slice(0, -3);
+        factorialFormula = factorialFormula.concat(") + ");
+      });
+      factorialFormula = factorialFormula.slice(0, -3);
+      var fractionalFormula = "(a/(a+b) + b/(a+b))^2 = (a^2)/((a+b)^2) + (ab)/((a+b)^2) + (ab)/((a+b)^2) + (b^2)/((a+b)^2)"
+      var a = String(f[0]);
+      var b = String(f[1]);
+      var ab = String(f[0]*f[1]);
+      var absum = String(Number(f[0])+Number(f[1]));
+      var factorialPlugIn = 
+        "("+a+"+"+b+")"+"^2 = "+a+"^2 + "+ab+" + "+ab+" + "+b+"^2 = "
+        +String(Math.pow(f[0], n))+" + "+ab+" + "+ab+" + "+String(Math.pow(f[1], n));
+      
+      var fractionalPlugIn = "("+a+"/"+absum+"+"+b+"/"+absum+")"+"^2 = "
+      setState(prevState => ({
+        ...prevState,
+        factorialFormula: factorialFormula,
+        fractionalFormula: fractionalFormula,
+        factorialPlugIn: factorialPlugIn,
+        fractionalPlugIn: fractionalPlugIn
+      }))
+    }
+    return;
+  }
 
   const handleChange = (e) => {
     const {id , value} = e.target   
@@ -18,36 +75,39 @@ function Ch12Generator() {
 
   const doAction = event => {
     event.preventDefault();
-    var f = String(state.formula);
-    
-    if(f.includes("+")) {
-      var exponent = 1;
-      var arr;
-      if(f.includes("(")) {
-        if(!f.includes(")")) {
-          alert("You must include an end parenthesis! Try again");
-          return;
-        }
-      }
-      arr = f.replace(" ","");
-      if(arr.includes("^")) {
-        exponent = Number(arr.split("^")[1]);
-        arr = arr.split("^")[0];
-      }
-      arr = arr.split("+");
-      var evaluation = 0;
-      arr.forEach(element => {
-        element = element.replace("(", "");
-        element = element.replace(")", "");
-        evaluation += Number(element);
-      });
-      var finalEvaluation = Math.pow(evaluation, exponent);
-      var results = f+" = "+String(finalEvaluation);
+    var f = String(state.input);
+    var n = 1;
+    if (!f.includes("^")) {
       setState(prevState => ({
         ...prevState,
-        result: results
+        warning: "You entry must be of the form (a+b+...+m)^n with at least two terms and n > 1"
       }))
+      return;
+    } else if (!f.includes("(")) {
+      setState(prevState => ({
+        ...prevState,
+        warning: "You entry must be of the form (a+b+...+m)^n with at least two terms and n > 1"
+      }))
+      return;
+    } else if (!f.includes(")")) {
+      setState(prevState => ({
+        ...prevState,
+        warning: "You entry must be of the form (a+b+...+m)^n with at least two terms and n > 1"
+      }))
+      return;
+    } else if (!f.includes("+")) {
+      setState(prevState => ({
+        ...prevState,
+        warning: "You entry must be of the form (a+b+...+m)^n with at least two terms and n > 1"
+      }))
+      return;
     }
+
+    n = Number(f.split("^")[1]);
+    var f2 = f.split("^")[0];
+    var f3 = f2.replace("(", "").replace(")", "");
+    var f4 = f3.split("+");
+    DistributivePower(n, f4);
 
   }
 
@@ -60,14 +120,14 @@ function Ch12Generator() {
               <h1>Chapter 12 Generator</h1>
               <h3>Instructions</h3>
               <p>
-                Enter some polynomial formula of the form (a+b+c+...+m)^n.
+                Enter some polynomial input of the form (a+b+c+...+m)^n.
               </p>
               <br />
               <Row className="align-items-bottom justify-content-md-center">
                 <Col></Col>
                 <Col className="col-9">
-                  <Form.Group controlId="formula">
-                    <Form.Control type="text" value={state.formula} onChange={handleChange} placeholder="formula">
+                  <Form.Group controlId="input">
+                    <Form.Control type="text" value={state.input} onChange={handleChange} placeholder="ex. (2+3)^3">
                     </Form.Control>
                   </Form.Group>
                 </Col>
@@ -76,12 +136,37 @@ function Ch12Generator() {
                 </Col>
               </Row>
             </Form>
-            <Row className="justify-content-md-center">
+            {/* <Row className="justify-content-md-center">
               <h3>Results:</h3>
+            </Row> */}
+            <Row className="justify-content-md-center">
+              <Col>
+                <p>{state.warning}</p>
+              </Col>
             </Row>
             <Row className="justify-content-md-center">
               <Col>
-                <h4>{state.result}</h4>
+                <p>{state.factorialFormula}</p>
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <Col>
+                <p>{state.factorialPlugIn}</p>
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <Col>
+                <p>{state.fractionalFormula}</p>
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <Col>
+                <p>{state.fractionalPlugIn}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p>{state.result}</p>
               </Col>
             </Row>
           </Card.Body>
