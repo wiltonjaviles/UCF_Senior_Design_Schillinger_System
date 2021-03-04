af930b25e8a6d9e916753fd93609879d16697603
Duke <luckystarnova@gmail.com>
2021-03-03 21:03:57 -0500

    Front of 9 mostly done; alg needs work

--- schillinger-system/src/components/Recordings.js
+++ schillinger-system/src/components/Recordings.js
@@ -1,24 +1,23 @@
 import '.././Style.css';
-<<<<<<< HEAD
-import GroupingPeriodicities from './applets/GroupingPeriodicities'
+import Ch3Generator from './applets/Ch3Generator'
 import Ch2Generator from './applets/Ch2Generator'
 import Ch4Generator from './applets/Ch4Generator'
 import Ch5Generator from './applets/Ch5Generator'
 import Ch6Generator from './applets/Ch6Generator'
-=======
+import Ch9Generator from './applets/Ch9Generator';
 import isAuthenticated from '.././components/authUser';
->>>>>>> de96495adf3b95ec106d1ad548d294538e5d281e
 
 function Recordings() {
   isAuthenticated();
   return (
     <div>
       <h1>Recordings Page</h1>
-      <GroupingPeriodicities />
       <Ch2Generator />
+      <Ch3Generator />
       <Ch4Generator />
       <Ch5Generator />
       <Ch6Generator />
+      <Ch9Generator />
     </div>
   );
 }
