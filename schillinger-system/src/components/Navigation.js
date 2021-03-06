5697aeeeea8377eb30a28049d7058dc32813875b
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-03-06 14:37:37 -0500

    renamed navbar brand to match website URL

--- schillinger-system/src/components/Navigation.js
+++ schillinger-system/src/components/Navigation.js
@@ -69,7 +69,7 @@ function Navigation() {
     <Navbar collapseOnSelect expand="lg">
         <Navbar.Brand>
           <Link to="/" className="nav-link-item">
-            <h5>Schillinger System</h5>
+            <h5>Learn Schillinger</h5>
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
