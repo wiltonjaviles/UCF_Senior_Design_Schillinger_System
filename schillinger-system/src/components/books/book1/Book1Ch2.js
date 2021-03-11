e33d9b857a0034e70a2ca4a1f38fb39c4e6851bb
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-03-11 16:06:01 -0500

    added speech for ch 1 and 2

--- schillinger-system/src/components/books/book1/Book1Ch2.js
+++ schillinger-system/src/components/books/book1/Book1Ch2.js
@@ -1,6 +1,7 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import '../../.././Style.css';
+import Speech from 'react-speech';
 import Ch2Generator from '../../applets/Ch2Generator';
 
 function Book1Ch2() {
@@ -11,6 +12,75 @@ function Book1Ch2() {
         <h1>Book One: Theory of Rhythm</h1>
         <a className="chapter-section-link" href="/book1ch2#1.2" id="1.2"><h3>Chapter 2. Interferences of Periodicities</h3></a>
         <a className="content-links" href="/book1ch2schillinger"><h5>View the source text.</h5></a>
+        <Speech 
+          textAsButton={true}
+          displayText="Listen to this text" 
+          text="
+          Chapter 2. Interfences of Periodicities
+
+          Now we shall discuss combining one periodicity (say, 3, 3, 3, 3) with another (say, 4, 4, 4, 4), called interference.
+          The periodicity with larger numbers (in our case, 4) will be called the “major generator”; the smaller will be called the “minor generator”. The way we will express two synchronized generators producing one interference group is a ÷ b *. The resultant of interference is r a ÷ b.
+          
+          Binary Synchronization
+          
+          To synchronize two periodicities, you must:
+          1. Find common product or common denominator (c.p. or c.d.)
+          2. Find complementary factors of both generators; the complementary factor of ā is (a b)÷a=b, and the complementary factor of b is (a b)÷b=a.
+          After this, you must draw a graph of both generators. To find the resultant (r), draw vertical lines from all attack points on both generators. The resultant is found by drawing lines through these points, placing accents over intersecting points. Then the common product is written, numbers are drawn out, and the whole thing is translated into musical notation.
+          When A equals any number and b equals 1, the resultant (r) represents a musical “bar”. The formula for a musical bar is:
+          T=r a ÷ 1
+          (the musical bar (T) is the resultant of a to one)
+          Example:
+          *image*
+          *note: sometimes Schillinger uses ÷ to indicate the a and b relationship, but also sometimes uses colon to indicate the relationship instead. These both mean interference
+          Here is the example in numbers, graph, and musical notation:
+          *image*
+          The resultant differs from b with respect to the accent in r (because both generators had an attack there).
+          This establishes a bar with a numerator of 2 i.e. 2/2, 2/4, etc. When the bar is 2/2, 1/2 = half note, when the bar is 2/4, 1/2 = quarter note.
+          Another example:
+          *image*
+          This case shows musical bars with 3 as a numerator. I.e.:
+          *image*
+          A third example:
+          *image*
+          The importance of this is that even the most noted composers do not seem to know that an accent should not be forced but should result from the superimposition of ā on b.
+          When it comes to the application of higher numerators (5, 7, 11, etc) that the music becomes incomprehensible to the average listener and the composer is the one to blame. When it comes to the shifting of accents which aren’t correctly expressed (through a and b), the performance suffers and listeners are confused.
+          Non-uniform rhythmic resultants occur when b ≠ 1. With the procedure we described above, you can find all rhythmic patterns used in the past, present, and future.
+          Example:
+          *image*
+          
+          Grouping
+          
+          There are three forms of grouping:
+          1. Grouping by c.p. In this case, c.p. = 6
+          *image*
+          2. Superimposition of a. 
+          a = 3. In this case, c.p. is excluded and b becomes merely an optional component.
+          *image*
+          3. Superimposition of b. 
+          b = 2. c.p. is excluded; ā becomes optional
+          *image*
+          Another Example:
+          *image*
+          *image*
+          *image*
+          *image*
+          These show the natural nucleus of a musical score, in which c.d. Units are arpeggio or obligato figures, a and b are chords, r rhythms of the theme, and c.p. Sustained tones (“pedla point”).
+          Resultants have the following characteristics:
+          Recurrence
+          Balance
+          Contrasts
+          Inversion
+          Thus, harmony of form is a product of physical efficiency.
+          All rhythmic patterns in music are either complete or incomplete resultants.
+          When all of the resultants up to a = 9 have been found, you can create all rhythm patterns in the past, present, and even the future.
+          All the necessary generators are:
+          *image*
+          When c.p. is greater than 15, use ā or b superimposition only.
+          When the numbers get large, a musical eighth note is the best denominator. All reducible fractions have been excluded from the above chart.
+          The a and b components show how ‘cross-rhythms’ should be performed. Beat a and b with both hands, listen to the resultant.
+          " 
+        />
           <p class="bookText">Now we shall discuss combining one periodicity (say, 3, 3, 3, 3) with another (say, 4, 4, 4, 4), called interference.</p>
           <p class="bookText">The periodicity with larger numbers (in our case, 4) will be called the “major generator”; the smaller will be called the “minor generator”. The way we will express two synchronized generators producing one interference group is a ÷ b *. The resultant of interference is r<sub>a ÷ b</sub>.</p>
         <a className="chapter-section-link" href="/book1ch2#1.2.a" id="1.2.a"><h5>A. Binary Synchronization</h5></a>
@@ -89,5 +159,4 @@ function Book1Ch2() {
     </div>
   );
 }
-
 export default Book1Ch2;
