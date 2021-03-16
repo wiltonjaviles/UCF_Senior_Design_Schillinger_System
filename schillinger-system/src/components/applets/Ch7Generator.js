4cd23769d73656b2f6be43837f3d80f0ae1ca55f
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-03-15 20:49:16 -0400

    still messing

--- schillinger-system/src/components/applets/Ch7Generator.js
+++ schillinger-system/src/components/applets/Ch7Generator.js
@@ -1,6 +1,7 @@
 import {Container, Row, Col, Form, Card, Button} from 'react-bootstrap';
 import React, { useState } from 'react';
 import '../.././Style.css';
+import abcjs from "abcjs";
 
 function Ch7Generator() {
   const [state , setState] = useState({
@@ -21,12 +22,13 @@ function Ch7Generator() {
 
   const generateR = event => {
     event.preventDefault();    
-    var A = String(state.variableA);
-    var B = String(state.variableB);
+    var A = Number(state.variableA);
+    var B = Number(state.variableB);
+
 
     setState(prevState => ({
         ...prevState,
-        output : A+"÷"+B
+        output : A+"÷"+B+"="
       }))
   }
 
@@ -45,8 +47,7 @@ function Ch7Generator() {
                 <Row className="form-row justify-content-md-center">
                 <Col className="col-2">
                   <Form.Group controlId="variableA">
-                    <Form.Control as="select" defaultValue="2" value={state.variableA} onChange={handleSelect}>
-                      <option>2</option>
+                    <Form.Control as="select" defaultValue="3" value={state.variableA} onChange={handleSelect}>
                       <option>3</option>
                       <option>4</option>
                       <option>5</option>
@@ -62,8 +63,7 @@ function Ch7Generator() {
                 </Col>
                 <Col className="col-2">              
                   <Form.Group controlId="variableB">
-                    <Form.Control as="select" defaultValue="1" value={state.variableB} onChange={handleSelect}>
-                      <option>1</option>
+                    <Form.Control as="select" defaultValue="2" value={state.variableB} onChange={handleSelect}>
                       <option>2</option>
                       <option>3</option>
                       <option>4</option>
@@ -131,3 +131,121 @@ function Ch7Generator() {
 
 export default Ch7Generator;
 
+function sMakeR(a,b) {
+  let arr = new Array();
+  for(let i=0; i<5; i++) {
+      arr[i] = new Array();
+    }
+
+  for(let i=0; i<a*b; i++) {
+      arr[0].push(1);
+  }
+  arr[1].push(a*b);
+  for(let i=1; i<a*b; i++) {
+      arr[1].push('');
+  }
+  for(let i=0; i<b; i++) {
+      arr[2].push(a);
+      for(let j=1; j<a; j++) {
+          arr[2].push('');
+      }
+  }
+  for(let i=0; i<a; i++) {
+      arr[3].push(b);
+      for(let j=1; j<b; j++) {
+          arr[3].push('');
+      }
+  }
+  let j=1;
+  for(let i=a*b-1; i>-1; i--) {
+      
+      if(arr[2][i] !== '' || arr[3][i] !== '') {
+          arr[4][i] = j;
+          j=1;
+      } else {j++; arr[4][i]='';}
+  }
+
+  return arr;
+}
+
+function simpleToABC(arrIn, measureLength) {
+  let measure = Number(0);
+  let longNote = Number(0);
+    
+    
+    let arrOut = new Array();
+    for(let i=0; i<arrIn.length; i++) {
+        arrOut[i] = new Array();
+    }
+
+    for(let i=0; i<arrIn.length; i++) {
+        measure = measureLength;
+        for(let j=0; j<arrIn[i].length; j++) {
+          
+          if(arrIn[i][j] !== ''){
+            if(arrIn[i][j] <= measure) {
+              arrOut[i].push(pushNote(arrIn[i][j]));
+              measure = measure - arrIn[i][j];
+            } else if(arrIn[i][j] > measure) {
+                if(arrIn[i][j] >= measureLength) {
+                  
+                  arrOut[i].push(pushNote(measure)+'-|');
+                  longNote = arrIn[i][j] - measure;
+                  for(longNote; longNote > 0; longNote=longNote-measureLength) {
+                    arrOut[i].push(pushNote(measureLength)+'-|');
+                  }
+                  if(longNote > 0) {
+                    arrOut[i].push(pushNote(longNote));
+                  } else {
+                    arrOut[i].pop();
+                    arrOut[i].push(pushNote(measureLength)+'|');
+                  }
+                  measure = measureLength - longNote;
+                } else {
+                  arrOut[i].push(pushNote(measure)+'-|'+pushNote(arrIn[i][j]-measure));
+                  measure = measureLength - (arrIn[i][j]-measure);
+                }
+            }
+            
+            if(measure === 0) {
+              arrOut[i].push('|');
+              measure = measureLength;
+            }
+
+          }
+        }
+    }
+    return arrOut;
+}
+
+function pushNote(a) {
+  if(a === 5) {
+    return 'A4-A1';
+  } else if(a > 8) {
+    let output = new Array(['A8-']);
+    let count = a-8;
+    while(count > 8) {
+      output.push('A8-');
+      count = count-8;
+    }
+    output.push('A'+count);
+    return output.toString();
+  } else {return 'A'+a;}
+}
+
+function lcm(x, y) {
+  if (isNaN(x) || isNaN(y)) 
+   return false;
+ return Math.abs((x * y) / gcd(x, y));
+}
+
+function  gcd(x, y) {
+ x = Math.abs(x);
+ y = Math.abs(y);
+ while(y) {
+   var t = y;
+   y = x % y;
+   x = t;
+ }
+ return x;
+}
