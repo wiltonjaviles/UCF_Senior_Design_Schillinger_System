06ceea8a863e3d5c8bc838a45e0484b9b73e66b3
Duke <luckystarnova@gmail.com>
2021-03-18 11:24:09 -0400

    file for playback (nothing in it yet)

---
+++ schillinger-system/src/components/applets/PlaybackTemplate.js
@@ -0,0 +1,15 @@
+import {Container, Row, Col, Form, Card, Button} from 'react-bootstrap';
+import React, { useState } from 'react';
+import '../.././Style.css';
+import abcjs from 'abcjs';
+
+function Playback(image, abc) {
+  
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+  
+  export default Playback;
