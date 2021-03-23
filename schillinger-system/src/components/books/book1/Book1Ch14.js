5aea380c672ef6a2c60bada3ba5a68163df92aae
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-03-23 18:28:44 -0400

    added ch14 applet to page

--- schillinger-system/src/components/books/book1/Book1Ch14.js
+++ schillinger-system/src/components/books/book1/Book1Ch14.js
@@ -1,6 +1,7 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import '../../.././Style.css';
+import Ch14Generator from '../../applets/Ch14Generator';
 
 function Book1Ch14() {
   return (
@@ -14,7 +15,8 @@ function Book1Ch14() {
         <a className="chapter-section-link" href="/book1#1.14.b" id="1.14.b"><h5>B. Acceleration in Non-uniform Groups</h5></a>
         <a className="chapter-section-link" href="/book1#1.14.c" id="1.14.c"><h5>C. Rubato</h5></a>
         <a className="chapter-section-link" href="/book1#1.14.d" id="1.14.d"><h5>D. Fermata</h5></a>
-        <br /> 
+        <Ch14Generator />
+        <br />
         <Row>
           <Col><Link to="/book1ch13" className="content-links"><h5>Previous Chapter</h5></Link></Col>
           <Col><Link to="/book2" className="content-links"><h5>Next Book</h5></Link></Col>
