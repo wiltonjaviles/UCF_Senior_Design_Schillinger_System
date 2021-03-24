1705db615bc8e83827cb605bd56c9684c906e0d0
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-03-24 17:15:27 -0400

    ch 10 template created

--- schillinger-system/src/components/books/book1/Book1Ch10.js
+++ schillinger-system/src/components/books/book1/Book1Ch10.js
@@ -12,12 +12,12 @@ function Book1Ch10() {
         <a className="chapter-section-link" href="/book1ch10#1.10" id="1.10"><h3>Chapter 10. Generalization of Variation Techniques</h3></a>
         <a className="content-links" href="/book1ch10schillinger"><h5>View the source text.</h5></a>
         <a className="chapter-section-link" href="/book1ch10#1.10.a" id="1.10.a"><h5>A. Permutations of the Higher Order</h5></a>
+        <Ch10Generator />
         <br /> 
         <Row>
           <Col><Link to="/book1ch9" className="content-links"><h5>Previous Chapter</h5></Link></Col>
           <Col><Link to="/book1ch11" className="content-links"><h5>Next Chapter</h5></Link></Col>
         </Row>
-        <Ch10Generator />
         <br />
       </Container>
     </div>
