97da94f4d237f9fa24c3ccecffe3b2f32842ec1f
sleepiestuff <malaniz21@knights.ucf.edu>
2021-03-28 22:49:11 +0000

    production prefix commit

--- server/index.js
+++ server/index.js
@@ -9,18 +9,24 @@ const http = require('http');
 const https = require('https');
 require('dotenv').config();
 const uri = process.env.MONGO_URI
+const path = require('path')
 
 app.use(express.urlencoded());
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../schillinger-system/client/build')))
+app.get('*', (req,res) => {
+	res.sendFile(path.join(__dirname + '../schillinger-system/client/build'))
+})
 
-const PORT = 443;
+
+const PORT = 27017;
 const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(uri);
+const client = new MongoClient("mongodb://schillAdmin:schillp4ssword%21@206.189.226.215:27017/schillinger-DB?authSource=admin&w=1");
 
 client.connect();
 
-mongoose.connect(uri, { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect("mongodb://localhost:27017/schillinger-DB?authSource=admin", {user:'schillAdmin', pass:'schillp4ssword!', useNewUrlParser: true, useFindAndModify: false });
 
 mongoose.connection.once('open', function() {
 	console.log('Connected to the Database.');
@@ -29,8 +35,22 @@ mongoose.connection.on('error', function(error) {
 	console.log('Mongoose Connection Error: ' + error);
 })
 
-app.get('/', (req, res) => {
-	res.send("hello world");
+// app.get('/', (req, res) => {
+// 	res.send("hello world");
+// })
+
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PATCH, DELETE, OPTIONS'
+	);
+	next();
 })
 
 app.use('/api', userRouter);
@@ -45,13 +65,13 @@ if (process.env.NODE_ENV === "production") {
 		ca: ca
 	};
 
-	https.createServer(credentials, app).listen(443, () => {
-		console.log('HTTPS Server running on port 443')
+	https.createServer(credentials, app).listen(403, () => {
+		console.log('HTTPS Server running on port 403')
 	});
 	// http.createServer( function(req, res) {
 	// 	res.writeHead(301, {"Location": "https://" + req.headers['host'] + req.url});
 	// 	res.end();
 	// }).listen(80);
 } else {
-	app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
 }
