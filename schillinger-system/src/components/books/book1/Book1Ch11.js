a359b7466b2c0f98d923cafeefd68dac646689aa
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-03-30 16:03:01 -0400

    ch 11 generator progress

--- schillinger-system/src/components/books/book1/Book1Ch11.js
+++ schillinger-system/src/components/books/book1/Book1Ch11.js
@@ -12,12 +12,11 @@ function Book1Ch11() {
         <a className="chapter-section-link" href="/book1ch11#1.11" id="1.11"><h3>Chapter 11. Composition of Homogeneous Rhythmic Continuity</h3></a>
         <a className="content-links" href="/book1ch11schillinger"><h5>View the source text.</h5></a>
         <br /> 
+        <Ch11Generator />
         <Row>
           <Col><Link to="/book1ch10" className="content-links"><h5>Previous Chapter</h5></Link></Col>
           <Col><Link to="/book1ch12" className="content-links"><h5>Next Chapter</h5></Link></Col>
         </Row>
-        <br />
-        <Ch11Generator />
         <br />
       </Container>
     </div>
