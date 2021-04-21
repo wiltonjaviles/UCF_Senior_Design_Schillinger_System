5ce7519f956abed5f187a70294e9197ff68e364e
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-04-21 19:58:59 -0400

    output alignment plus prettier output

--- schillinger-system/src/components/applets/Ch6Generator.js
+++ schillinger-system/src/components/applets/Ch6Generator.js
@@ -65,17 +65,29 @@ function Ch6Generator() {
     let outArr = simpleToABC(sMakeTrinomialR(vA,vB,vC),vG);
     let abcOutR = "X:1\nK:C\n"+outArr[5].join("")+"\n";
     // let abcOutR1 = "X:1\nK:C\n"+outArr[9].join("")+"\n";
+    var abc = "X:1\nK:C\nV: V1 clef=treble\nV: V2 clef=treble\nV: V3 clef=treble\nV: V4 clef=treble\nV: V5 clef=treble";
+    abc = abc+"\nV: V6 clef=treble\nV: V7 clef=treble\nV: V8 clef=treble\nV: V9 clef=treble\nV: V10 clef=treble"
+    abc = abc+"\n[V: V1]"+outArr[0].join("");
+    abc = abc+"\n[V: V2]"+outArr[1].join("");
+    abc = abc+"\n[V: V3]"+outArr[2].join("");
+    abc = abc+"\n[V: V4]"+outArr[3].join("");
+    abc = abc+"\n[V: V5]"+outArr[4].join("");
+    abc = abc+"\n[V: V6]"+outArr[5].join("");
+    abc = abc+"\n[V: V7]"+outArr[6].join("");
+    abc = abc+"\n[V: V8]"+outArr[7].join("");
+    abc = abc+"\n[V: V9]"+outArr[8].join("");
+    abc = abc+"\n[V: V10]"+outArr[9].join("");
 
-    abcjs.renderAbc("outputABC1", "X:1\nK:C\n"+outArr[0].join("")+"\n");
-    abcjs.renderAbc("outputABC2", "X:1\nK:C\n"+outArr[1].join("")+"\n");
-    abcjs.renderAbc("outputA", "X:1\nK:C\n"+outArr[2].join("")+"\n");
-    abcjs.renderAbc("outputB", "X:1\nK:C\n"+outArr[3].join("")+"\n");
-    abcjs.renderAbc("outputC", "X:1\nK:C\n"+outArr[4].join("")+"\n");
-    abcjs.renderAbc("outputR", "X:1\nK:C\n"+outArr[5].join("")+"\n");
-    abcjs.renderAbc("outputComplementA", "X:1\nK:C\n"+outArr[6].join("")+"\n");
-    abcjs.renderAbc("outputComplementB", "X:1\nK:C\n"+outArr[7].join("")+"\n");
-    abcjs.renderAbc("outputComplementC", "X:1\nK:C\n"+outArr[8].join("")+"\n");
-    abcjs.renderAbc("outputR1", "X:1\nK:C\n"+outArr[9].join("")+"\n");
+    abcjs.renderAbc("outputABC1", abc);
+    // abcjs.renderAbc("outputABC2", "X:1\nK:C\n"+outArr[1].join("")+"\n");
+    // abcjs.renderAbc("outputA", "X:1\nK:C\n"+outArr[2].join("")+"\n");
+    // abcjs.renderAbc("outputB", "X:1\nK:C\n"+outArr[3].join("")+"\n");
+    // abcjs.renderAbc("outputC", "X:1\nK:C\n"+outArr[4].join("")+"\n");
+    // abcjs.renderAbc("outputR", "X:1\nK:C\n"+outArr[5].join("")+"\n");
+    // abcjs.renderAbc("outputComplementA", "X:1\nK:C\n"+outArr[6].join("")+"\n");
+    // abcjs.renderAbc("outputComplementB", "X:1\nK:C\n"+outArr[7].join("")+"\n");
+    // abcjs.renderAbc("outputComplementC", "X:1\nK:C\n"+outArr[8].join("")+"\n");
+    // abcjs.renderAbc("outputR1", "X:1\nK:C\n"+outArr[9].join("")+"\n");
     
 
     setState(prevState => ({
@@ -163,84 +175,52 @@ function Ch6Generator() {
                 <Col className="col-3">
                   <Button variant="secondary" type="submit" className="float-right" onClick={generateR}>Generate</Button>
                 </Col>
-              </Row>
-                
+              </Row>                
                 <Row className="justify-content-md-center">
-                    <Col className="col-3">
-                        <h4>R: </h4>
-                    </Col>
-                    <Col className="col-8">
-                        <h4>{state.OutputR}</h4>
-                    </Col>
-                </Row>
-                
+                  <Col className="col-3">
+                    <h4>R: </h4>
+                  </Col>
+                  <Col className="col-8">
+                    <h4>{state.OutputR}</h4>
+                  </Col>
+                </Row>                
                 <Row className="justify-content-md-center">
-                    <Col className="col-3">
-                        <h4>R': </h4>
-                    </Col>
-                    <Col className="col-8">
-                        <h4>{state.OutputR1}</h4>
-                    </Col>
+                  <Col className="col-3">
+                    <h4>R': </h4>
+                  </Col>
+                  <Col className="col-8">
+                    <h4>{state.OutputR1}</h4>
+                  </Col>
                 </Row>
                 <Row className="justify-content-md-center">
-                    
-                      <div id="outputABC1"></div>
-                      
-                    
+                   <div id="outputABC1"></div>
                 </Row>
                 <Row className="justify-content-md-center">
-                    
-                      <div id="outputABC2"></div>
-                      
-                    
+                  <div id="outputABC2"></div>
                 </Row>
                 <Row className="justify-content-md-center">
-                    
-                      <div id="outputA"></div>
-                      
-                    
+                  <div id="outputA"></div>
                 </Row>
                 <Row className="justify-content-md-center">
-                    
-                      <div id="outputB"></div>
-                      
-                    
+                  <div id="outputB"></div>
                 </Row>
                 <Row className="justify-content-md-center">
-                    
-                      <div id="outputC"></div>
-                      
-                    
+                  <div id="outputC"></div>
                 </Row>
                 <Row className="justify-content-md-center">
-                    
-                      <div id="outputR"></div>
-                      
-                    
+                  <div id="outputR"></div>
                 </Row>
                 <Row className="justify-content-md-center">
-                    
-                      <div id="outputComplementA"></div>
-                      
-                    
+                  <div id="outputComplementA"></div>
+                </Row>
+                <Row className="justify-content-md-center">                    
+                  <div id="outputComplementB"></div>                  
                 </Row>
                 <Row className="justify-content-md-center">
-                    
-                      <div id="outputComplementB"></div>
-                      
-                    
+                  <div id="outputComplementC"></div>
                 </Row>
                 <Row className="justify-content-md-center">
-                    
-                      <div id="outputComplementC"></div>
-                      
-                    
-                </Row>
-                <Row className="justify-content-md-center">
-                    
-                      <div id="outputR1"></div>
-                      
-                    
+                  <div id="outputR1"></div>
                 </Row>
                 <Playback abc = {state.abcString}/>
             </Form>
