0b1c26864a01375c5025f9f38e03eee3aab7ecfb
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-04-22 16:43:23 -0400

    tts weird spelling error

--- schillinger-system/src/components/books/book1/Book1Ch8Schillinger.js
+++ schillinger-system/src/components/books/book1/Book1Ch8Schillinger.js
@@ -12,8 +12,146 @@ function Book1Ch8Schillinger() {
         <br />
         <TextToSpeech 
           text="
-          
-          "
+          CHAPTER 8
+          COORDINATION OF TIME STRUCTURES
+Motion, that is, changeability in time is the most important intrinsic property of music. Diﬁerent cultures of different geographical and historical localities have developed many types and forms of intonation. The latter varies greatly in tuning, in quantity of pitches employed, in quantity of simultaneous parts, and in the ways of treating them.
+The types are as diversiﬁed as drum—beats, instrumental and vocal monody (one part music), organum, discantus, counterpoint, harmony, combinations of melody and harmony, combinations of counterpoint and harmony, different forms of coupled voices, simultaneous combinations of several harmonies, and many others. Any of these types—as well as any combinations of them—constitute the different musical cultures. In each case, musical culture crystallizes itself into a deﬁnite combination of types and forms of intonation. The latter crystallize into habits and traditions.
+For example, people belonging to a harmonic musical culture want every melody harmonized. But people belonging to a monodic musical culture are disturbed by the very presence of harmony. Music of one culture may be music (meaningful sound) to the members of that culture; but the very same music may be noise (meaningless sound) to the members of another. The functionality of music is comparable to a great extent to that of a language.
+Nevertheless, all forms of music have one fundamental property in common: organized time. The plasticity of the temporal structure of music, as expressed through its attacks and durations, deﬁnes the quality of music. Different types and forms of intonation, as well as different types of musical instruments, come and go like the fashions, while the everlasting strife for temporal plasticity remains a symbol of the eternal in music.
+The temporal structure of music, usually known as rhythm, pertains to two directions: simultaneity and continuity. The rhythm of simultaneity is a form of coordination among the different components (parts). The rhythm of continuity is a form of coordination of the successive moments of one component (part).
+People of our civilization have developed the power of reasoning at the price of losing many of the instincts of primitive man. Europeans have never possessed the instinct of rhythm with which the Africans are endowed. So called European classical music has never attained the ideal it strived for, that ideal being: the utmost plasticity of the temporal organization. When J. S. Bach, for example, tried to develop a coordinated independence of simultaneous parts, he succeeded in producing only a resultant which is uniformity.* We ﬁnd evidence of the same failures in Mozart and Beethoven. But a score in which the several coordinated parts produce, together, a resultant which has a distinct pattern—has been a lost art of the aboriginal African drummers. The age of this art can probably be counted in tens of thousands of years!
+(*That is to say, when the separate rhythms of the separate parts of a Each score are added up, the result tends to be simple uniformity. Schillinger suggests the desirability of scores, and develops a method of scoring, so that the separate parts, while satisfactory rhythmically by themselves. all add up to a new rhythm which is not uniformity.)
+Today in the United States, due to the transplantation of Africans to this continent, there is a renaissance of rhythm. Habits form quickly—and the instinct of rhythm in the present American generation surpasses anything known throughout European history. Yet our professional coordinators of rhythm, speciﬁcally in the ﬁeld of dance music, are slaves to, rather than masters of, rhythm. There is plenty of evidence that the urge for coordination of the whole through individualized parts is growing. The so-called pyramids (sustained arpeggio produced by successive entrances of several instruments) is but an incompetent attempt to solve the same problem.
+Fortunately, we do not have to feel discouraged or moan over this lost art. The power of reasoning offers us a complete scientiﬁc solution.
+This problem can be formulated as the distribution of a duration-group through instrumental and attack-groups.
+The entire technique consists of ﬁve successive operations with respect to the following:
+(1) The number of individual parts in a score;
+(2) The quantity of attacks appearing with each individual part in suc-
+cess10n;
+(3) The rhythmic patterns for each individual part;
+(4) The coordination of all parts (which become the resultants of instrumental interference) into a form which, in turn, results in a speciﬁed rhythmic pattern (the resultant of interference of all parts); and
+(5) The application of such scores to any type of musical measures (bars).
+Any part of such a score can be treated as melody, coupled melody, block-harmony, harmony, instrumental ﬁguration—or as a purely percussive (drum) part. Aside from the temporal structure of the score, the practical uses of this technique in intonation depend on the composer’s skill in the respective ﬁelds concerned, i.e., melody, harmony, counterpoint and orchestration.
+A. DISTRIBUTION OF A DURATION-GROUP (T) THROUGH INSTRUMENTAL (i) AND ATTACK (a) GROUPS.
+Notation
+pli number of places in the instrumental group.
+pla number of places in the attack-group.
+aa number of attacks in the attack-group.
+aT number of attacks in the duration-group.
+PL the ﬁnal number of places.
+A the synchronized attack—group (the number of attacks synchronized with the number of places).
+A’ the ﬁnal attack group (number of attacks synchronized with the duration group).
+T the original duration-group.
+T’ the synchronized duration~group.
+T the ﬁnal duration—group.
+NT the number of ﬁnal duration—groups.
+Procedures:
+Interference between the_ number of places in the instrumental group (pli)
+and the number of places in the attack-group (pla).
+PL =pli/pla; pla(pli) Pli(pla)
+The product of the number of attacks in the attack group (aa) by the
+complementary factor to the number of places in the attack-group (pli
+after reduction).
+A = a_aXpli
+Interference between the synchronized attack-group (A) and the number
+of attacks in the original duration-group (aT).
+A’= A/aT=a_aXpli/aT
+The product of the original duration-group (T) by the complementary
+factor to its number of attacks (A’).
+T’ = T-A’ = T xa_a pli/aT
+Interference between the synchronized duration-group (T’) and the ﬁnal
+duration—group (T).
+NT=T’/T
+B. SYNCHRONIZATION OF AN ATTACK-GROUP (a) WITH A DURATION-GROUP (T).
+Distribution of attacks of an attack-group (a_a) through the number of attacks of a duration group (aT).
+First Case: <a_a over a_T equals one>
+Example:
+<figure 58>
+Second Case
+Example:
+Figure 59.
+Third Case: i.e., a reducible fraction
+Example:
+a_a = 6a; T = r3+2 = 6t; aT = 4a
+Figure 60
+C. DISTRIBUTION OF A SYNCHRONIZED DURATION-GROUP (T’) THROUGH THE
+FINAL DURATION-GROUP (T‘).
+First Case: 
+Example:
+T’ = 6t; T = 6t
+Figure 61.
+Second Case:
+Example :
+T’=6t; T=5t
+N5t = 6
+Figure 62.
+Third Case i.e., a reducible traction
+Example:
+T’ = 6t; T = 4t
+Figure 63.
+Example :
+D. SYNCHRONIZATION OF AN INSTRUMENTAL GROUP (pli) WITH AN ATTACK-GROUP (pla)
+Example:
+Pli = 4;
+pla= 3;
+a_a=3+2+3=8;
+T=R5÷2=10t; 
+6a
+Figure 66
+Example:
+pli=3:
+Pla =3;
+a_a=3+2+2+2+3=10;
+T=r4÷3 =16t;
+10a
+Figure 67
+Example:
+pli = 6;
+pla = 8;
+a_a= r5÷4= 20; 
+T = r4÷3=16t; 
+10a T=8t
+Example of composition of the resultant of instrumental interference.
+pl(i) =pl(a)=2
+Form Of distribution : 5+3
+Figure 68
+(1)
+(2) 2 is an equivalent of 5+3 = 8
+(3) Duration-group: 
+(4) 10t X4 = 40t
+(5) 
+Preliminary Scoring
+Final Scoring
+Example of composition of the resultant of instrumental interference.
+P1(i) = 3 Mil) = 3
+Form of distribution: 8+3+5+2
+Figure 70.
+(1)  = 1
+(2) 4 is an equivalent of 8+3+5+2=18
+18X1 = 18
+(3) Duration group: r 5+2 =10t 1'33— = 3 3(6)
+aT = 6
+(4) 10f X 3 = 30t
+(5) When T = 8/8, 30t/8+ 16/4; 15x4/4=15T
+Preliminary Scoring
+Figure 71 (continued).
+Figure 71 (concluded).
+Final Scoring
+Figure 72 (continued)
+Figure 72 (concluded).
+Example of composition of the resultant of instrumental interference.
+pl(i) = 6; p1(a) = 8;
+Form of distribution: r5+4
+figure 73.
+(1) 
+(2) 8 is equivalent to 20 in r5÷4 20 X 3 = 60
+(3) Duration-group = r4÷3 aT = 10; 60/10=6 6(10)
+(4) 16 X 6 = 96t; a given T = 8/8
+(5)96t/8t = 12T
+Preliminary Scoring
+Figure 74
+Final Scoring
+"
         />
         <br />
         <img className="img-fluid img-thumbnail" src="images/book1/ch8/screenshots/book1ch8_01.png" alt="book1ch8img1"></img>
