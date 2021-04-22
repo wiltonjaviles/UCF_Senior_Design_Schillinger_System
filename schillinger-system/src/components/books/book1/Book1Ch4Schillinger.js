84b2d9761ef40a75d13325020b57119a6e234d12
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-04-22 10:57:14 -0400

    tts

--- schillinger-system/src/components/books/book1/Book1Ch4Schillinger.js
+++ schillinger-system/src/components/books/book1/Book1Ch4Schillinger.js
@@ -10,7 +10,11 @@ function Book1Ch4Schillinger() {
         <br /> 
         <h1>Schillinger Source Text for Book 1 Chapter 4</h1>
         <br />
-        <TextToSpeech />
+        <TextToSpeech 
+          text="
+          
+          "
+        />
         <br />
         <img className="img-fluid img-thumbnail" src="images/book1/ch4/screenshots/book1ch4_01.png" alt="book1ch4img1"></img>
         <img className="img-fluid img-thumbnail" src="images/book1/ch4/screenshots/book1ch4_02.png" alt="book1ch4img2"></img>
--- schillinger-system/src/components/books/book1/Book1Ch5Schillinger.js
+++ schillinger-system/src/components/books/book1/Book1Ch5Schillinger.js
@@ -10,7 +10,11 @@ function Book1Ch5Schillinger() {
         <br /> 
         <h1>Schillinger Source Text for Book 1 Chapter 5</h1>
         <br />
-        <TextToSpeech />
+        <TextToSpeech 
+          text="
+          
+          "
+        />
         <br />
         <img className="img-fluid img-thumbnail" src="images/book1/ch5/screenshots/book1ch5_01.png" alt="book1ch5img1"></img>
         <img className="img-fluid img-thumbnail" src="images/book1/ch5/screenshots/book1ch5_02.png" alt="book1ch5img2"></img>
