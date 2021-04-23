e0f1c32f92ddd8267e0964cc4291bdde85d4dcac
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-04-22 21:47:07 -0400

    changed up playback

--- schillinger-system/src/components/applets/TextToSpeech.js
+++ schillinger-system/src/components/applets/TextToSpeech.js
@@ -1,8 +1,9 @@
-import {Container, Row, Button, ButtonToolbar, ButtonGroup} from 'react-bootstrap' 
+import {Container, Card, Row, Button, ButtonToolbar, ButtonGroup} from 'react-bootstrap' 
 import '../.././Style.css';
 
 function TextToSpeech(props) {
   var isPlaying = false;
+  var isPaused = false;
   var text = String(props.text);
   var speech = new SpeechSynthesisUtterance(text);
   const playText = () => {
@@ -19,19 +20,41 @@ function TextToSpeech(props) {
     isPlaying = false;
   }
 
+  const pauseText = () => {
+    speechSynthesis.pause();
+    isPlaying = true;
+  }
+
+  const resumeText = () => {
+    if(isPaused) {
+      speechSynthesis.resume();
+    }
+  }
+
   return (
     <div>
       <Container>
-        <Row className="justify-content-center">
-          <ButtonToolbar>
-            <ButtonGroup className="mr-2">              
-              <Button className="btn-secondary btn-sm" onClick={playText}>Listen to this text</Button>
-            </ButtonGroup>
-            <ButtonGroup>              
-              <Button className="btn-secondary btn-sm" onClick={stopText}>Stop listening</Button>
-            </ButtonGroup>
-          </ButtonToolbar>
-        </Row>
+        <Card className="playback-card">
+          <Card.Body className="mt-0 mb-0">
+            <Row className="m-0 justify-content-center"><p className="m-0"><b>Listen to this text:</b></p></Row>
+            <Row className="justify-content-center">
+              <ButtonToolbar>
+                <ButtonGroup className="mr-2 mt-0 mb-0">              
+                  <Button className="btn-secondary btn-sm" onClick={playText}>Play</Button>
+                </ButtonGroup>
+                <ButtonGroup>              
+                  <Button className="btn-secondary btn-sm mr-2 mt-0 mb-0" onClick={stopText}>Stop</Button>
+                </ButtonGroup>
+                <ButtonGroup className="mr-2 mt-0 mb-0">              
+                  <Button className="btn-secondary btn-sm" onClick={pauseText}>Pause</Button>
+                </ButtonGroup>
+                <ButtonGroup>              
+                  <Button className="btn-secondary btn-sm mt-0 mb-0" onClick={resumeText}>Resume</Button>
+                </ButtonGroup>
+              </ButtonToolbar>
+            </Row>
+          </Card.Body>
+        </Card>
       </Container>
     </div>
   );
