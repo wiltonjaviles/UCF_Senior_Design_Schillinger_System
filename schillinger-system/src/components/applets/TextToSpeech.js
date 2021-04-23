1326cbf5030d81ad87e5d7fa919c070a96aaefed
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-04-22 21:49:42 -0400

    recentered

--- schillinger-system/src/components/applets/TextToSpeech.js
+++ schillinger-system/src/components/applets/TextToSpeech.js
@@ -34,27 +34,29 @@ function TextToSpeech(props) {
   return (
     <div>
       <Container>
-        <Card className="playback-card">
-          <Card.Body className="mt-0 mb-0">
-            <Row className="m-0 justify-content-center"><p className="m-0"><b>Listen to this text:</b></p></Row>
-            <Row className="justify-content-center">
-              <ButtonToolbar>
-                <ButtonGroup className="mr-2 mt-0 mb-0">              
-                  <Button className="btn-secondary btn-sm" onClick={playText}>Play</Button>
-                </ButtonGroup>
-                <ButtonGroup>              
-                  <Button className="btn-secondary btn-sm mr-2 mt-0 mb-0" onClick={stopText}>Stop</Button>
-                </ButtonGroup>
-                <ButtonGroup className="mr-2 mt-0 mb-0">              
-                  <Button className="btn-secondary btn-sm" onClick={pauseText}>Pause</Button>
-                </ButtonGroup>
-                <ButtonGroup>              
-                  <Button className="btn-secondary btn-sm mt-0 mb-0" onClick={resumeText}>Resume</Button>
-                </ButtonGroup>
-              </ButtonToolbar>
-            </Row>
-          </Card.Body>
-        </Card>
+        <Row className="justify-content-center">
+          <Card className="playback-card">
+            <Card.Body className="mt-0 mb-0">
+              <Row className="m-0 justify-content-center"><p className="m-0"><b>Listen to this text:</b></p></Row>
+              <Row className="justify-content-center">
+                <ButtonToolbar>
+                  <ButtonGroup className="mr-2 mt-0 mb-0">              
+                    <Button className="btn-secondary btn-sm" onClick={playText}>Play</Button>
+                  </ButtonGroup>
+                  <ButtonGroup>              
+                    <Button className="btn-secondary btn-sm mr-2 mt-0 mb-0" onClick={stopText}>Stop</Button>
+                  </ButtonGroup>
+                  <ButtonGroup className="mr-2 mt-0 mb-0">              
+                    <Button className="btn-secondary btn-sm" onClick={pauseText}>Pause</Button>
+                  </ButtonGroup>
+                  <ButtonGroup>              
+                    <Button className="btn-secondary btn-sm mt-0 mb-0" onClick={resumeText}>Resume</Button>
+                  </ButtonGroup>
+                </ButtonToolbar>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Row>
       </Container>
     </div>
   );
