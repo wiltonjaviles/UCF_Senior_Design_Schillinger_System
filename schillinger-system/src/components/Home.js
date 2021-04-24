6ff66f4a4e738c6dd058c0643e40ccd69e8deda5
Sleepie <malaniz21@knights.ucf.edu>
2021-04-24 11:34:53 -0400

    removed redundant functions and libraries from the home page

--- schillinger-system/src/components/Home.js
+++ schillinger-system/src/components/Home.js
@@ -1,33 +1,10 @@
 import '.././Style.css';
 // import isAuthenticated from '.././components/authUser';
 import React from 'react';
-import { Container, Button, ButtonGroup, OverlayTrigger, Row, Col, Tooltip } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
 import TextToSpeech from "./applets/TextToSpeech";
 
-// function to retrieve and parse applet history items
-function getItems(arrName) {
-  var schillArr_data = JSON.parse(localStorage.getItem(arrName));
-  return schillArr_data;
-}
-
-// function to clear local storage (clear applet history button)
-function clearStorage() {
-  localStorage.clear();
-  window.location.reload();
-}
-
 function Home() {
-
-  if (localStorage.getItem("schillArr") == null) {
-
-    // make a new array to hold our schillinger applet history, place it in localStorage
-    return localStorage.setItem('schillArr', '["empty","empty","empty","empty","empty","empty","empty","empty","empty","empty"]');
-  
-  }
-  
-  // get the current applet history array items
-  var schillArr_data = getItems("schillArr");
-
   return (
     <div>
       <Container>
