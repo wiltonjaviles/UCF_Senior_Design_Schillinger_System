8c9dcee071b8a84f9dc8ad727b55970aab5c23d0
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-04-24 14:08:50 -0400

    tts typos

--- schillinger-system/src/components/books/book1/Book1Ch14Schillinger.js
+++ schillinger-system/src/components/books/book1/Book1Ch14Schillinger.js
@@ -18,39 +18,39 @@ function Book1Ch14Schillinger() {
         By introducing constant velocities into the art forms we try to simplify the scheme of surrounding phenomena. But different forms of progressive variable speed are also well known to us. through biological growth, through gravity, through different series of acceleration and different ratios of acceleration.
         The ratios of acceleration through gravity grow rapidly. Series like the natural harmonic series reveal a much greater graduality in their rates of acceleration. The urge for freedom in a musical performance often reveals itself by speeding up or slowing down a certain passage written out in musical notation as a uniform group. The increase and the decrease of speed may be however, merely two reciprocals of the same series.
         Rhythms based on constant velocities are either continuous repetitions of the terms of a monomial periodicity, or of several monomial or binomial periodicities synchronized. But rhythms based on variable velocities or progressions are single terms belonging to different periodicities.
-        In the following list of different mathematical series, the series of gravity has been eliminated as it is too intense” for esthetic purposes. The simpler the ratios of acceleration, the more obvious the forms of acceleration will appear to the listener of music. Such is the case of doubling or quadrupling the original speed. In all three forms of representation—number, graph and music—there is a constant speed which reveals various forms of acceleration through the actual number-values Music expressed in such a way may be performed or conducted in a constant tempo, counting out all the durations exactly as they are represented by the number-values.
+        In the following list of different mathematical series, the series of gravity has been eliminated as it is too intense” for esthetic purposes. The simpler the ratios of acceleration, the more obvious the forms of acceleration will appear to the listener of music. Such is the case of doubling or quadrupling the original speed. In all three forms of representation—number, graph and music—there is a constant speed which reveals various forms of acceleration through the actual number-values. Music expressed in such a way may be performed or conducted in a constant tempo, counting out all the durations exactly as they are represented by the number-values.
         Here is a list of various series of acceleration.
         (1) Natural Harmonic Series.
-        1,2,3,4,5,6,7,8,9. . . .
+        1, 2, 3, 4, 5, 6, 7, 8, 9. . . .
         (2) Arithmetical Progressions.
         + n constant
-        +2 —-1,3,5,7,9....
-        +3 ——1,4,7,10,13...
+        +2 —-1, 3, 5, 7, 9....
+        +3 ——1, 4, 7, 10, 13...
         (3) Geometrical Progressions.
         X n
-        X2  - 1,2,4,8,16....
-        X3 - 1,3,9,27
-        X2 - 3,6,12,24,48.
+        X2  - 1, 2, 4, 8, 16....
+        X3 - 1, 3, 9, 27
+        X2 - 3, 6, 12, 24, 48.
         X3  - 2, 6, 18, 54.
         (4) Power Series.
         nth power
         2, 4, 8-, 16, 32
-        3, 9,27, 81 .
+        3, 9, 27, 81 .
         5, 25, 125. . .
         (5) Summation Series.
-        1, 2, 3,5, 8, 13, 21. .
-        1, 3,4, 7, 11, 18. .
-        1,4,5, 9, 14, 23 . .
+        1, 2, 3, 5, 8, 13, 21. .
+        1, 3, 4, 7, 11, 18. .
+        1, 4, 5, 9, 14, 23 . .
         (6) Arithmetical Progressions with Variable Differences.
         1+1, 2+2, 4+3, 7+4, n+5, 16+, 22+7 . .
         (7) Prime Number Series.
-        1, 2, 3, 5, 7,11,13, 17, 19, 23, 29, 31, 37 . . . .
-        It is important to note that the rate of acceleration varies within each given series. For instance, in the first series, the ratio between the second and the first value is <2%1>, while the ratio between the sixth and the seventh number values is <7%6>. Therefore, when a greater speed of acceleration is desired, it is advisable to take a number—value considerably higher than unity. This concerns mostly the type of series that grow with greater speed, such as arithmetical progressions.
-        Although any series may be used for carrying out different types of accelerando and rallentando, the most suitable are those belonging to a given rhythmic family. For example, to obtain a proper type of accel. rall.” in the <M.8/8> series (Charleston” family), we should use the most suitable series, which is the first summation series—for all the number—values of this series represent generators of the <M.8/8> series. Likewise, for any music written in the 4 4 series, such as marches, polonaises, mazurkas, etc., we shall have the most appropriate accel. rall. expressed through the second summation series. Similarly, music in the
-        <M.9/9> series requires the third summation series for its accel. Rall.
+        1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 . . . .
+        It is important to note that the rate of acceleration varies within each given series. For instance, in the first series, the ratio between the second and the first value is 2 divided by 1, while the ratio between the sixth and the seventh number values is 7 divided by 6. Therefore, when a greater speed of acceleration is desired, it is advisable to take a number—value considerably higher than unity. This concerns mostly the type of series that grow with greater speed, such as arithmetical progressions.
+        Although any series may be used for carrying out different types of accelerando and rallentando, the most suitable are those belonging to a given rhythmic family. For example, to obtain a proper type of accelerando and rallentando.” in the 8 8 series (Charleston” family), we should use the most suitable series, which is the first summation series—for all the number—values of this series represent generators of the 8 8 series. Likewise, for any music written in the 4 4 series, such as marches, polonaises, mazurkas, etc., we shall have the most appropriate accel. rall. expressed through the second summation series. Similarly, music in the
+        9 9 series requires the third summation series for its accelerando and rallentando.
         In many cases the freedom of the performer leads to the use of numbers alien to the series in which a given piece of music is composed; this causes an obvious dissatisfaction and many listeners —with a natural sense of rhythm— feel that something is wrong but cannot explain the cause of such rhythmic irregularity. Speeding up and slowing down is a natural tendency in much folk music. Some of the most striking examples may be found in Hungarian music (see Liszt’s Second Hungarian Rhapsody) and music of gypsies in various countries.
         The technique of variable speeds becomes extremely important in dealing with stage productions, compositions for the dance, and especially film music.
-        In film music, the animation technique in particular requires absolute precision of timing. In illustrating a chase, one has to time the corresponding music in such a fashion that the whole period of acceleration will be limited to a definite portion of time, with the precision of <M.1/24> of a second (the duration of a single shot in a film synchronized with sound). In some instances of descriptive music, especially those dealing with the speeding up and slowing down of mechanisms, similar precision adds a great deal to the esthetic effects. This method permits the use of the accel. and rall. of the same rate and series as counter-rhythms, as well as the resultants of their interference (we shall call them the resultant. of acceleration).
+        In film music, the animation technique in particular requires absolute precision of timing. In illustrating a chase, one has to time the corresponding music in such a fashion that the whole period of acceleration will be limited to a definite portion of time, with the precision of 1/24 of a second (the duration of a single shot in a film synchronized with sound). In some instances of descriptive music, especially those dealing with the speeding up and slowing down of mechanisms, similar precision adds a great deal to the esthetic effects. This method permits the use of the accel. and rall. of the same rate and series as counter-rhythms, as well as the resultants of their interference (we shall call them the resultant. of acceleration).
         Thus, we acquire four fundamental forms to be used as material for acceleration groups.
         (1) Increasing velocity (accelerando).
         (2) Decreasing velocity (rallentando).
@@ -58,8 +58,8 @@ function Book1Ch14Schillinger() {
         (4) The resultant of acceleration.
         Forms (1) and (2) may be used for the introductions and conclusions (codas); forms (3) and (4) for the climaxes.
         In either case it is more practical to find the decreasing velocity (increasing number-values) first, as it is more practical to have a definite initial number-value.
-        For example, if we use the natural harmonic series and start with unity, we find a practical stopping point at 8, because 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36. The sum must be in a simple relation to the multiples of the group-unit (measure). The value 36 offers a number of possibilities. Four-and-a-half of 4 4 time (in eighths); nine bars in 4 4 time (in quarters); four bars of 9 8 time (in eighths); six bars in <M.6/8> time (in eighths); twelve bars in  3 4 time (in quarters).
-        Using the first summation series for accelerando in the <M.8/8> family, we obtain a practical value of 32 by summing up 1 + 2 + 3 + 5 + 8 + 13. This offers exactly four bars of <M.8/8> time, and in music of eight-bar groups. the ratio becomes very simple (<M.8/4> = <M.2/1>).
+        For example, if we use the natural harmonic series and start with unity, we find a practical stopping point at 8, because 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36. The sum must be in a simple relation to the multiples of the group-unit (measure). The value 36 offers a number of possibilities. Four-and-a-half of 4 4 time (in eighths); nine bars in 4 4 time (in quarters); four bars of 9 8 time (in eighths); six bars in 6 8 time (in eighths); twelve bars in  3 4 time (in quarters).
+        Using the first summation series for accelerando in the 8 8 family, we obtain a practical value of 32 by summing up 1 + 2 + 3 + 5 + 8 + 13. This offers exactly four bars of 8 8 time, and in music of eight-bar groups. the ratio becomes very simple (8 4 = 2 1).
         A. ACCELERATION IN UNIFORM GROUPS.
         Examples.
         B. ACCELERATION IN NON-UNIFORM GROUPS.
@@ -76,11 +76,11 @@ function Book1Ch14Schillinger() {
         The process of unbalancing a balanced binomial must be carried out by means of a unit of deviation. This unit of deviation, supposedly an infinitesimal (in the calculus, dx) becomes a rational fraction in the field of musical rhythm. The most satisfactory results are produced by means of a standard unit of deviation, which is defined in this theory as M.1/t i.e., the unit of a series of fractional continuity. We shall call it <tau> (the Greek letter, tau).
         Formula for a standard unit of deviation.
         tau = 1/t
-        Example I. 
-        Take the second theme of Chopin’s Valse in c# minor. All bars of this theme have the following construction;  3 4 . Many performers play the first bar of this theme like this. . Let us see what causes the transformation of , i.e., 2 + 2 into  , i.e., 3 +1. The way this binomial is written makes it 2 4 + 2 4. In this case we have a 4 4 series, where <tau = 1/4>. Therefore, the process of unbalancing the original binomial (2 4 + 2 4) may be expressed as follows.
+        Example 1. 
+        Take the second theme of Chopin’s Valse in c# minor. All bars of this theme have the following construction;  3 4 . Many performers play the first bar of this theme like this. . Let us see what causes the transformation of 2 + 2 into 3 +1. The way this binomial is written makes it 2 4 + 2 4. In this case we have a 4 4 series, where <tau = 1/4>. Therefore, the process of unbalancing the original binomial (2 4 + 2 4) may be expressed as follows.
         which means .
         Example II.
-        Take any foxtrot where you find 4 4  in the printed copies. In a performance you hear it as  or . Let us follow the previous procedure. The two half-notes in a fox-trot time belong in reality to the 8 8 series. They must be expressed as 8 8 = 4 8 + 4 8. In this case tau = 1 8. By adding 1 8 to the first term and subtracting from the second we obtain (4 8 + 1 8) + (4 8 - 1 8) = 5 8 + 3 8.
+        Take any foxtrot where you find 4 4  in the printed copies. In a performance you hear it as. Let us follow the previous procedure. The two half-notes in a fox-trot time belong in reality to the 8 8 series. They must be expressed as 8 8 = 4 8 + 4 8. In this case tau = 1 8. By adding 1 8 to the first term and subtracting from the second we obtain (4 8 + 1 8) + (4 8 minus 1 8) = 5 8 + 3 8.
         In both examples the process of unbalancing may be reversed, i.e., 1 4 + 3 4 and 3 8 + 5 8.
         The process of balancing an unbalanced binomial is a typical case of the ratio simplification as we find it in swing” performance. Write  in 4 4 time, and the swingsters” will play it  all in the same 4 4 time. The same thing happens in boogie—woogie,” where the written accompaniment of broken octaves 8 8  is played 4 4 , and the upper parts are handled as Charleston.” The ratio 3 + 1 becomes 2 + 1, which is closer to balance.
         All the other forms of rubato” playing are either small groups of accelerando and rallentando, or are the same as.
@@ -88,22 +88,21 @@ function Book1Ch14Schillinger() {
         There are two types of fermata; the two may seem to have an entirely different character, but in reality this difference is purely quantitative. The first type produces the effect of a full stop. It is commonly used at the very beginning, at the very end, at the moment of a climax, or before a new theme enters.
         In writing out such a fermata, it is best to make it a simple multiple of the preceding or the following values, or the sum of the preceding group of uniform values.
         An example of transcription of a fermata of the first type.
-        figure 150.
         The first four bars move in halves, the following two—in wholes. By assigning a double value to the last note, we satisfy two requirements. One. we produce a simple ratio of 1 + 1 + 2 in the last three bars, i.e. our last note is the simplest multiple for each of the two preceding notes; two. such a multiple compensates the first four bars, thus creating a balance 4 + 4.
         Transcription.
         Obviously, this gives the utmost satisfaction.
         The second type of fermata is a temporary delay. The method of creating simple ratios is most effective in transcribing this type of fermata. Here is a transcription of the following example.
-        By isolating the group preceding the fermata we obtain by isolating the group with a fermata we obtain 
+        By isolating the group preceding the fermata we obtain
         Now the entire group appears as follows.
         This being transcribed into number-values gives.
         The first bar is related to the second bar as 3 by 1 because 3 4 = 6 8; 6 8 by 2 8 = 3 8 by 1 8.
         Simplifying the ratio 3 by 1 into 2 by 1, we obtain the following musical Measures. 3 4 + 3 8. Making the first duration in the second bar (the fermata) longer, we obtain a binomial 2 8 + 1 8 or in musical notation.
-        This procedure makes the absolute value of the fermata note increase in
-        a very subtle way, 1 16 longer than the original duration. Here are the numbers.
-        from the musical transcription.
+        This procedure makes the absolute value of the fermata note increase in a very subtle way, 1 16 longer than the original duration. Here are the numbers from the musical transcription.
         By comparing the original and the transcription, it is easy to see that the fermata note (which originally was 3 16) became 4 16, thus gaining 1 16.
         The rhythm of variable velocities presents a fascinating field for study and exploration. The very thought that various rhythmic groups may speed up and slow down at various rates, appearing and disappearing, is overwhelming.
         This idea stimulates one’s imagination towards the complex harmony of the universe, where different celestial bodies (comets, stars, planets, satellites) coexist in harmony of variable velocities.
+
+        
         So ends the exposition of Schillinger’s theory of rhythm, to be followed next by the theory of pitch—scales. Although the casual reader may not be entirely aware of it, the specific techniques have now been set forth whereby all possible rhythms, of any nature whatsoever, may be derived. And these all possible” rhythms have been grouped into related families, sub-families and styles,” so that what is an infinity of rhythms may be rapidly and practically utilized in the actual composition of music. 
         "
         />
