a2359e9dbfa372390e7eb4d1f7772a934f484a61
Wilton Aviles <wiltonjaviles@gmail.com>
2021-04-24 11:31:28 -0400

    Added more stuff to hom page

--- schillinger-system/src/components/Home.js
+++ schillinger-system/src/components/Home.js
@@ -38,85 +38,23 @@ function Home() {
         <TextToSpeech />
         {/* <p>Welcome to the Schillinger System of Musical Composition!</p> */}
         {/* <br /> */}
-        <h2>Applet History</h2>
+        <h2>Introduction</h2>
+        <p>The <b>Schillinger System of Musical Composition</b>, named after Joseph Schillinger, is a method of musical composition based on mathematical processes. It comprises theories of rhythm, harmony, melody, counterpoint, form and semantics (emotional meaning, as in movie music). It offers a systematic and non-genre approach to music analysis and composition, a descriptive rather than prescriptive grammar of music. The Schillinger System might have served as a road map for many later developments in music theory and composition. Instead, it languished in relative obscurity <a href="https://en.wikipedia.org/wiki/Schillinger_System">(source)</a>.</p>
+        <p><b>Joseph Moiseyevich Schillinger</b> (1895-1943), was a composer, music theorist, and composition teacher; he was born in Kharkiv, in the Kharkov Governorate of the Russian Empire (present-day Ukraine) and died in New York City <a href="https://en.wikipedia.org/wiki/Joseph_Schillinger">(source)</a>.</p>
+        <img className="img-fluid img-thumbnail" src="images/schillinger/JosephSchillinger.png" alt="SchillingerPhoto"></img>
         <br />
-        <p>Applet History saves previous versions of your executed applets. These actions are ordered from most recent to least recent.</p>
-        <p>Hover the buttons below to see the book, chapter, and chapter title that each button corresponds to.</p>
-        <br />
-        
-        <Button variant="success" onClick={clearStorage}>
-          Clear Applet History
-        </Button>
-
         <br /> 
         <br />
-
-        <ButtonGroup>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[0].id} <br /> Title: {schillArr_data[0].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[0].id}`}>
-              1
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[1].id} <br /> Title: {schillArr_data[1].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[1].id}`}>
-              2
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[2].id} <br /> Title: {schillArr_data[2].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[2].id}`}>
-              3
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[3].id} <br /> Title: {schillArr_data[3].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[3].id}`}>
-              4
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[4].id} <br /> Title: {schillArr_data[4].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[4].id}`}>
-              5
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[5].id} <br /> Title: {schillArr_data[5].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[5].id}`}>
-              6
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[6].id} <br /> Title: {schillArr_data[6].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[6].id}`}>
-              7
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[7].id} <br /> Title: {schillArr_data[7].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[7].id}`}>
-              8
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[8].id} <br /> Title: {schillArr_data[8].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[8].id}`}>
-              9
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger overlay={<Tooltip>Book/Chapter: {schillArr_data[9].id} <br /> Title: {schillArr_data[9].title}</Tooltip>} placement="bottom">
-            <Button variant="success" href={`/${schillArr_data[9].id}`}>
-              {/* {schillArr_data[9].id} */}
-              10
-            </Button>
-          </OverlayTrigger>
-        </ButtonGroup>
-
-        <br /> 
-        <br />
-        <h1>About this project</h1>
+        <h2>About this project</h2>
         <Row>
           <p>
             This website was created as a senior design project for University of Central Florida's computer science 
             program.
             Our team is Michael Alaniz, Wilton Aviles, Michael Duke, and Kathryn Wyrick.
-            Our sponsor is Richard Leinecker.
+            Our sponsor is Dr. Richard Leinecker.
           </p>
         </Row>
-        <br />
+        
         <p>This project, once completed, will provide a comprehensive Schillinger System learning tool for musicians and software developers that can be used to create computer-assisted musical composition software. Since Schillinger never finished refining his ideas for the System it fell into obscurity after his death. This project aims bring his system back to the forefront of musical composition. It could also provide substantial guidance for composers and software developers to implement Schillinger’s ideas, which has never been done before.</p>
         <p>In addition to the applications in composition software, this project will provide easy access to Schillinger’s system for people who simply want to learn music theory. Since it is more math-based than the traditional system, Schillinger’s System may be easier for some people to understand.</p>
         <p>There is also a small chance that because our project revives interest in Schillinger’s System of Musical Composition, in time it could replace the existing system of composing music. A clear understanding of his ideas could result in a shift in how music is written to his system.</p>
@@ -125,25 +63,28 @@ function Home() {
         <br />
         <h3>Meet the Team:</h3>
         <Row>
-          <Col><h5>Richard Leinecker</h5></Col>
-          <Col><h5>Michael Alaniz</h5></Col>
-          <Col><h5>Wilton Aviles</h5></Col>
-          <Col><h5>Michael Duke</h5></Col>
-          <Col><h5>Kathryn Wyrick</h5></Col>
+          <Col><h4>Dr. Richard Leinecker</h4><br/><h5>Sponsor</h5><br/><img className="img-fluid img-thumbnail" src="images/teamphotos/RichardLeinecker.png" alt="LeineckerPhoto"></img></Col>
+          <Col><br/><br/><p>Rick has worked on a wide variety of projects as lead software engineer. This includes enterprise-level applications as well as kernel-level security applications as senior software engineer at MCI. Rick started life as a musician, playing in the Miami philharmonic. Beginning as an undergraduate, he has written hundreds of fine pieces of music for large orchestra, small ensemble, and soloists. Rick does research on a regular basis. Much of it is within the area of data compression. He also researches computer assisted music composition, and is currently writing about Hindemith music composition and the Schillinger system of musical composition.</p></Col>
         </Row>
+        <Row><br/></Row>
         <Row>
-          <Col><img className="img-fluid img-thumbnail" src="images/teamphotos/RichardLeinecker.png" alt="LeineckerPhoto"></img></Col>
-          <Col><img className="img-fluid img-thumbnail" src="images/teamphotos/MichaelAlaniz.png" alt="AlanizPhoto"></img></Col>
-          <Col><img className="img-fluid img-thumbnail" src="images/teamphotos/WiltonAviles.png" alt="AvilesPhoto"></img></Col>
-          <Col><img className="img-fluid img-thumbnail" src="images/teamphotos/MichaelDuke.png" alt="DukePhoto"></img></Col>
-          <Col><img className="img-fluid img-thumbnail" src="images/teamphotos/KathrynWyrick.png" alt="WyrickPhoto"></img></Col>
+          <Col><h4>Michael Alaniz</h4><br/><h5>Full Stack Float</h5><br/><img className="img-fluid img-thumbnail" src="images/teamphotos/MichaelAlaniz.png" alt="AlanizPhoto"></img></Col>
+          <Col><br/><br/><p>Stuff about Michael</p></Col>
         </Row>
+        <Row><br/></Row>
         <Row>
-          <Col><p>About Leinecker</p></Col>
-          <Col><p>About Michael</p></Col>
-          <Col><p>About Wilton</p></Col>
-          <Col><p>About Duke</p></Col>
-          <Col><p>About Kathryn</p></Col>
+          <Col><h4>Wilton Aviles</h4><br/><h5>Project Manager</h5><br/><img className="img-fluid img-thumbnail" src="images/teamphotos/WiltonAviles.png" alt="AvilesPhoto"></img></Col>
+          <Col><br/><br/><p>About Wilton</p></Col>
+        </Row>
+        <Row><br/></Row>
+        <Row>
+          <Col><h4>Michael Duke</h4><br/><h5>Document Lead, Applet Design</h5><br/><img className="img-fluid img-thumbnail" src="images/teamphotos/MichaelDuke.png" alt="DukePhoto"></img></Col>
+          <Col><br/><br/><p>Stuff about Duke</p></Col>
+        </Row>
+        <Row><br/></Row>
+        <Row>
+          <Col><h4>Kathryn Wyrick</h4><br/><h5>Front End, Applet Implementation</h5><br/><img className="img-fluid img-thumbnail" src="images/teamphotos/KathrynWyrick.png" alt="WyrickPhoto"></img></Col>
+          <Col><br/><br/><p>Stuff About Kathryn</p></Col>
         </Row>
       </Container>
     </div>
