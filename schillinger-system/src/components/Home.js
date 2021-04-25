cf9a44956f88a8d23abca991c2b901b42f3387a5
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-04-25 11:09:01 -0400

    tts typos

--- schillinger-system/src/components/Home.js
+++ schillinger-system/src/components/Home.js
@@ -19,32 +19,32 @@ function Home() {
           It comprises theories of rhythm, harmony, melody, counterpoint, form and semantics (emotional meaning, as in movie music). 
           It offers a systematic and non-genre approach to music analysis and composition, a descriptive rather than prescriptive grammar of music. 
           The Schillinger System might have served as a road map for many later developments in music theory and composition. 
-          Instead, it languished in relative obscurity .
-          Joseph Moiseyevich Schillinger (1895-1943), was a composer, music theorist, and composition teacher; he was born in Kharkiv, in the Kharkov Governorate of the Russian Empire (present-day Ukraine) and died in New York City .
-          his website was created as a senior design project for University of Central Florida's computer science program. Our team is Michael Alaniz, Wilton Aviles, Michael Duke, and Kathryn Wyrick. Our sponsor is Dr. Richard Leinecker.
-          This project, once completed, will provide a comprehensive Schillinger System learning tool for musicians and software developers that can be used to create computer-assisted musical composition software. Since Schillinger never finished refining his ideas for the System it fell into obscurity after his death. This project aims bring his system back to the forefront of musical composition. It could also provide substantial guidance for composers and software developers to implement Schillinger’s ideas, which has never been done before.
+          Instead, it languished in relative obscurity.
+          Joseph Moiseyevich Schillinger (1895-1943), was a composer, music theorist, and composition teacher; he was born in Kharkiv, in the Kharkov Governorate of the Russian Empire (present-day Ukraine) and died in New York City.
+          his website was created as a senior design project for University of Central Florida's computer science program. Our team is Michael Alaniz, Wilton Aviles, Michael Duke, and Kathryn Wyrick. Our sponsor is Dr. Richard  lie necker.
+          This project, once completed, will provide a comprehensive Schillinger System learning tool for musicians and software developers that can be used to create computer-assisted musical composition software. Since Schillinger never finished refining his ideas for the System it fell into obscurity after his death. This project aims to bring his system back to the forefront of musical composition. It could also provide substantial guidance for composers and software developers to implement Schillinger’s ideas.
           In addition to the applications in composition software, this project will provide easy access to Schillinger’s system for people who simply want to learn music theory. Since it is more math-based than the traditional system, Schillinger’s System may be easier for some people to understand.
           There is also a chance that our project revives interest in Schillinger’s System of Musical Composition and in time it could replace the existing system of composing music. A clear understanding of his ideas could result in a shift in how music is written to his system.
           Meet the Team:
-          Dr. Richard Leinecker
+          Dr. Richard lie necker
           Sponsor
           Rick has worked on a wide variety of projects as lead software engineer. 
           This includes enterprise-level applications as well as kernel-level security applications as senior software engineer at MCI.
           Rick started life as a musician, playing in the Miami philharmonic. Beginning as an undergraduate, he has written hundreds of fine pieces of music for large orchestra, small ensemble, and soloists. Rick does research on a regular basis. Much of it is within the area of data compression. He also researches computer assisted music composition, and is currently writing about Hindemith music composition and the Schillinger system of musical composition.
           Michael Alaniz
           Full Stack Float
-          Michael Alaniz is a Computer Science graduate from UCF, Spring 2021, and was a member of the first team to work on the Learn Schillinger project. He was in charge of the Web Server implementation, management, and a Full-Stack float. Michael enjoys playing the piano, music production, machine learning, and deep learning. LinkedIn
+          Michael Alaniz is a Computer Science graduate from U C F, Spring 2021, and was a member of the first team to work on the Learn Schillinger project. He was in charge of the Web Server implementation, management, and a Full-Stack float. Michael enjoys playing the piano, music production, machine learning, and deep learning. LinkedIn
           Wilton Aviles
           Project Manager
           Wilton Aviles is a Computer Science graduate from the University of Central Florida, Spring 2021, and was a member of the first team to work on this project. He was the project manager and was in charge of filling the website, delegating tasks, design decisions, and making sure the team was prompt and completing tasks in a timely manner. He has enjoyed music since an early age, and has produced music for various musicians throughout his time in college.
-          He enjoys coding, music, video games, basketball, and being a student of life. Wilton was involved with the Engineering Leadership and Innovation Institute at UCF (eli2) as a Student Leader and Project Lead. LinkedIn
+          He enjoys coding, music, video games, basketball, and being a student of life. Wilton was involved with the Engineering Leadership and Innovation Institute at U C F (eli squared) as a Student Leader and Project Lead. LinkedIn
           Michael Duke
           Document Lead, Applet Design
           Michael Duke is a computer science student from Orlando. He also has a vocal performance bachelors, and has a wife and also a son, who at the time of writing is reasonably small. Duke uses his free time studying foreign languages and playing games.
           Kathryn Wyrick
           Front End, Applet Design
           Kathryn is a senior graduating in the class of Spring 2021 (woo!) and was a member of the first team to work on this project. She majored in Computer Science and minored in Mathematics. She was in charge of the initial website setup, including creating a skeleton of all the pages for the project, designing the navigation and routing, and creating some general-use components such as the text-to-speech, music playback, and templates for the other applets.
-          She also designed and implemented the waveform and pendulum applets and implemented several of the applets in Book 1. She has been interested in music her whole life and can play (with varying degrees of success) the violin, piano, ukulele, ocarina, melodica, and trumpet. Her cat's name is Mochi! After graduating, she has accepted a position with Lockheed Martin and will start working there in June 2021.
+          She also designed and implemented the waveform and pendulum applets and implemented several of the applets in Book 1. She has been interested in music her whole life and can play (with varying degrees of success) the violin, piano, ukulele, ocarina, melodica, and trumpet. Her cat's name is Mow chi! After graduating, she has accepted a position with Lockheed Martin and will start working there in June 2021.
           "
         />
         <br />
@@ -65,7 +65,7 @@ function Home() {
             Our sponsor is Dr. Richard Leinecker.
           </p>
         </Row>
-        <p>This project, once completed, will provide a comprehensive Schillinger System learning tool for musicians and software developers that can be used to create computer-assisted musical composition software. Since Schillinger never finished refining his ideas for the System it fell into obscurity after his death. This project aims bring his system back to the forefront of musical composition. It could also provide substantial guidance for composers and software developers to implement Schillinger’s ideas, which has never been done before.</p>
+        <p>This project, once completed, will provide a comprehensive Schillinger System learning tool for musicians and software developers that can be used to create computer-assisted musical composition software. Since Schillinger never finished refining his ideas for the System it fell into obscurity after his death. This project aims to bring his system back to the forefront of musical composition. It could also provide substantial guidance for composers and software developers to implement Schillinger’s ideas.</p>
         <p>In addition to the applications in composition software, this project will provide easy access to Schillinger’s system for people who simply want to learn music theory. Since it is more math-based than the traditional system, Schillinger’s System may be easier for some people to understand.</p>
         <p>There is also a small chance that because our project revives interest in Schillinger’s System of Musical Composition, in time it could replace the existing system of composing music. A clear understanding of his ideas could result in a shift in how music is written to his system.</p>
         <br />
