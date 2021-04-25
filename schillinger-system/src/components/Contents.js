bad7b7b2ca20ede04bb9dc2be8eb825982a74cbd
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-04-24 21:59:02 -0400

    strongbooks

--- schillinger-system/src/components/Contents.js
+++ schillinger-system/src/components/Contents.js
@@ -18,19 +18,19 @@ function Contents() {
             <hr />
             <Link to="/intro" className="content-links"><h5 className="text-justify">Introduction</h5></Link>
             <hr />
-            <Link to="/book1" className="content-links"><h5 className="text-justify">Book One: Theory of Rhythm</h5></Link>
+            <Link to="/book1" className="content-links"><h5 className="text-justify"><strong>Book One:</strong> Theory of Rhythm</h5></Link>
             <hr />
-            <Link to="/book2" className="content-links"><h5 className="text-justify">Book Two: Theory of Pitch-Scales (Coming Soon!)</h5></Link>
+            <Link to="/book2" className="content-links"><h5 className="text-justify"><strong>Book Two:</strong> Theory of Pitch-Scales (Coming Soon!)</h5></Link>
             <hr />
-            <Link to="/book3" className="content-links"><h5 className="text-justify">Book Three: Variations of Music by Means of Geometrical Projection  (Coming Soon!)</h5></Link>
+            <Link to="/book3" className="content-links"><h5 className="text-justify"><strong>Book Three:</strong> Variations of Music by Means of Geometrical Projection  (Coming Soon!)</h5></Link>
             <hr />
-            <Link to="/book4" className="content-links"><h5 className="text-justify">Book Four: Theory of Melody  (Coming Soon!)</h5></Link>
+            <Link to="/book4" className="content-links"><h5 className="text-justify"><strong>Book Four:</strong> Theory of Melody  (Coming Soon!)</h5></Link>
             <hr />
-            <Link to="/book5" className="content-links"><h5 className="text-justify">Book Five: Special Theory of Harmony  (Coming Soon!)</h5></Link>
+            <Link to="/book5" className="content-links"><h5 className="text-justify"><strong>Book Five:</strong> Special Theory of Harmony  (Coming Soon!)</h5></Link>
             <hr />
-            <Link to="/book6" className="content-links"><h5 className="text-justify">Book Six: The Correlation of Harmony and Melody  (Coming Soon!)</h5></Link>
+            <Link to="/book6" className="content-links"><h5 className="text-justify"><strong>Book Six:</strong> The Correlation of Harmony and Melody  (Coming Soon!)</h5></Link>
             <hr />
-            <Link to="/book7" className="content-links"><h5 className="text-justify">Book Seven: Theory of Counterpoint  (Coming Soon!)</h5></Link>
+            <Link to="/book7" className="content-links"><h5 className="text-justify"><strong>Book Seven:</strong> Theory of Counterpoint  (Coming Soon!)</h5></Link>
             <hr />
 
           </Col>
