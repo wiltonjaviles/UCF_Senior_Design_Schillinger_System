b2266164f5baeac7e7efaf9c7521726d289dfb0e
Kathryn Wyrick <kathryn.m.wyrick@gmail.com>
2021-04-24 22:25:29 -0400

    adjusting spacing

--- schillinger-system/src/components/books/book1/Book1Ch4.js
+++ schillinger-system/src/components/books/book1/Book1Ch4.js
@@ -66,56 +66,36 @@ function Book1Ch4() {
           <Row className="justify-content-center">
           <Col className="col-6"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img4.png" alt="book1ch4img4"></img></Col>
           </Row>
-          <br />
-          <br />
           <Row className="justify-content-center">
           <Col className="col-6"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img5.png" alt="book1ch4img5"></img></Col>
           </Row>
-          <br />
-          <br />
           <p class="bookText">Grouping by b produces syncopated rhythms. The resultant and the bar do not close at the same time during the first run of the resultant. This means the resultant needs to be repeated so they stop at the same time.</p>
           <p class="bookText">We can find when they will align from the following:</p>
           <Row className="justify-content-center">
           <Col className="col-4"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img6.png" alt="book1ch4img6"></img></Col>
           </Row>
-          <br />
-          <br />
           <p class="bookText">This will give us the number of bars the resultant will have. It will always have a remainder. The denominator of the remainder will tell us how many times the resultant will have to be repeated.</p>
           <Row className="justify-content-center">
           <Col className="col-4"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img7.png" alt="book1ch4img7"></img></Col>
           </Row>
-          <br />
-          <br />
           <Row className="justify-content-center">
           <Col className="col-6"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img8.png" alt="book1ch4img8"></img></Col>
           </Row>
-          <br />
-          <br />
           <Row className="justify-content-center">
           <Col className="col-6"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img9.png" alt="book1ch4img9"></img></Col>
           </Row>
-          <br />
-          <br />
           <Row className="justify-content-center">
           <Col className="col-6"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img10.png" alt="book1ch4img10"></img></Col>
           </Row>
-          <br />
-          <br />
           <Row className="justify-content-center">
           <Col className="col-6"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img11.png" alt="book1ch4img11"></img></Col>
           </Row>
-          <br />
-          <br />
           <Row className="justify-content-center">
           <Col className="col-6"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img12.png" alt="book1ch4img12"></img></Col>
           </Row>
-          <br />
-          <br />
           <Row className="justify-content-center">
           <Col className="col-6"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img13.png" alt="book1ch4img13"></img></Col>
           </Row>
-          <br />
-          <br />
           <Row className="justify-content-center">
           <Col className="col-6"><img className="img-fluid img-thumbnail" src="images/book1/ch4/img14.png" alt="book1ch4img14"></img></Col>
           </Row>
